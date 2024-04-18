475e317086742181887542263eab817cbe5795c0
Rimaito's Lab <rimaitos.lab@gmail.com>
2024-04-18 16:04:54 +0200

    default impl of read_to_eof

--- guardian-backup-domain/src/model/blobs/blob_fetch.rs
+++ guardian-backup-domain/src/model/blobs/blob_fetch.rs
@@ -3,5 +3,19 @@ pub trait BlobFetch {
 
     async fn read(&mut self, buf: &mut [u8]) -> Result<usize, Self::Error>;
 
-    async fn read_to_eof(self) -> Result<Box<[u8]>, Self::Error>;
+    async fn read_to_eof(mut self) -> Result<Box<[u8]>, Self::Error> {
+        let mut chunk = [0; 1024];
+        let mut res = Vec::new();
+
+        loop {
+            let read = self.read(&mut chunk)?;
+            if read == 0 {
+                break
+            }
+
+            res.extend_from_slice(chunk.split_at(read).0)
+        }
+
+        Ok(res.into())
+    }
 }
