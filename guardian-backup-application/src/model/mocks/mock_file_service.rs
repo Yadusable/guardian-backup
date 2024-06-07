c512d1c003a1ea1f49dd14a1d19d8e2d303927df
Rimaito's Lab <rimaitos.lab@gmail.com>
2024-06-07 08:32:49 +0200

    fix

--- guardian-backup-application/src/model/mocks/mock_file_service.rs
+++ guardian-backup-application/src/model/mocks/mock_file_service.rs
@@ -16,7 +16,7 @@ pub struct MockFileService {}
 
 impl FileService for MockFileService {
     type File = MockFile;
-    type Error = tokio::io::Error;
+    type Error = Infallible;
 
     async fn get_file(path: &Path) -> Result<Self::File, Self::Error> {
         Ok(MockFile {
@@ -39,6 +39,22 @@ impl FileService for MockFileService {
                 last_modified: 123456789,
             },
         })
+    }
+
+    async fn delete_file(path: &Path) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    async fn delete_dir_all(path: &Path) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    async fn write_file(
+        path: &Path,
+        file_meta: &FileMetadata,
+        blob: impl BlobFetch,
+    ) -> Result<(), Self::Error> {
+        Ok(())
     }
 }
 
